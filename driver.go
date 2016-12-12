b2c02b47ffd8c40f3d94c698a5f940831f503a6f
Adrian Cybulski <adrian@rtgnx.xyz>
2016-12-12 19:45:32 +0000

    indent

--- driver.go
+++ driver.go
@@ -191,7 +191,7 @@ func (d *glusterfsDriver) unmountVolume(target string) error {
 }
 
 func (d glusterfsDriver) Capabilities(r volume.Request) volume.Response {
-    var res volume.Response
-    res.Capabilities = volume.Capability{Scope: "local"}
-    return res
+	var res volume.Response
+	res.Capabilities = volume.Capability{Scope: "local"}
+	return res
 }
